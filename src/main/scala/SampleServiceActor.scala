eeab9c60904a809ce1b5379a578160d2c785952c
mervi <mervi.h.leino@gmail.com>
2016-11-11 20:40:51 +0200

    Added some CORS support

--- src/main/scala/SampleServiceActor.scala
+++ src/main/scala/SampleServiceActor.scala
@@ -1,5 +1,6 @@
 import akka.actor.Actor
 import spray.routing.HttpService
+import spray.http.HttpHeaders._ 
 
 class SampleServiceActor extends Actor with SampleRoute {
     def actorRefFactory = context
@@ -10,15 +11,38 @@ trait SampleRoute extends HttpService {
     import spray.httpx.SprayJsonSupport._
     import Todo._
     import spray.http.MediaTypes
-    
+
     var todos = Seq(
-        Todo(0, "Setup a dev environment", 0),
+        Todo(0, "Setup a dev environment", 2),
         Todo(1, "Learn React with Typescript", 1),
-        Todo(2, "Setup unit tests, Karma + shallow rendering?", 2)
+        Todo(2, "Setup unit tests, Karma + shallow rendering?", 2),
+        Todo(3, "Use UI library e.g. Material UI", 2),
+        Todo(4, "Create small backend with Scala, Akka, Spray and some NoSQL DB", 1),
+        Todo(5, "Integrate app with backend", 1),
+        Todo(6, "Learn redux", 0),
+        Todo(7, "Routing between states", 0),
+        Todo(8, "Add drag and drop feature for list items", 2)
     )
 
+    def addHeaders = respondWithHeaders(
+        RawHeader("Access-Control-Allow-Origin", "http://localhost:3000"),
+        RawHeader("Access-Control-Allow-Headers", "Content-Type"),
+        RawHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS"),
+        RawHeader("Access-Control-Max-Age", "86400"))
+
     val route = {
-         path("todo") {
+        addHeaders {
+            path("todos") {
+                respondWithMediaType(MediaTypes.`application/json`) {
+                    get {
+                        complete(todos)
+                    }
+                }
+            } ~
+            path("todo") {
+                options {
+                    complete("ok")
+                } ~
                 get {
                     parameters('id.as[Int]) { (id) =>
                         val todo = todos.find(_.id == id)
@@ -34,19 +58,16 @@ trait SampleRoute extends HttpService {
                 } ~
                 post {
                     entity(as[Todo]) { todo =>
-                        todos = todos :+ todo
-                        complete("New todo added!")
+                        val newTodo = Todo(scala.util.Random.nextInt, todo.description, todo.status)
+                        todos = todos :+ newTodo
+                        respondWithMediaType(MediaTypes.`application/json`) {
+                            complete {
+                                newTodo
+                            }
+                        }
                     }
                 }
-        } ~
-        path("todos") {
-            respondWithMediaType(MediaTypes.`application/json`) {
-                get {
-                    complete(todos)
-                }
             }
-        } ~ get {
-            complete("I exist!")
         }
     }
 }
