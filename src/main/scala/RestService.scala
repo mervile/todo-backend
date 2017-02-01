1bfe9137506a52f78d2f86540182b76b478ce75d
Mervi Leino <mervi.h.leino@gmail.com>
2017-02-01 19:25:27 +0200

    Added some minimal same origin checking

--- src/main/scala/RestService.scala
+++ src/main/scala/RestService.scala
@@ -15,7 +15,7 @@ import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 import scala.util.{Failure, Success}
 
-trait RestService extends TodosJSONSupport {
+trait RestService extends TodosJSONSupport with CORS {
   implicit val system:ActorSystem
   implicit val materializer:ActorMaterializer
   //implicit val executionContext:ExecutionContext
@@ -23,13 +23,6 @@ trait RestService extends TodosJSONSupport {
   implicit val timeout = Timeout(10.seconds)
 
   val todoService = system.actorOf(TodoServiceActor.props(),"todoService")
-
-  def addHeaders = respondWithDefaultHeaders(
-    RawHeader("Access-Control-Allow-Origin", "http://localhost:3000"),
-    RawHeader("Access-Control-Allow-Credentials", "http://localhost:3000"),
-    RawHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"),
-    RawHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE"),
-    RawHeader("Access-Control-Max-Age", "86400"))
 
   def myUserPassAuthenticator(credentials: Credentials): Option[ApiUser] =
     credentials match {
@@ -48,60 +41,62 @@ trait RestService extends TodosJSONSupport {
   val users = mutable.Map[String, ApiUser]()
   val route: Route = {
     handleRejections(myRejectionHandler) {
-      addHeaders {
-        options {
-          complete(StatusCodes.OK)
-        } ~
-        pathPrefix("auth") {
-          path("login") {
-            post {
-              formFields('username.as[String], 'password.as[String]) { (username, password) =>
-                val maybeUser = (todoService ? FindUser(username)).mapTo[Option[ApiUser]]
-                onSuccess(maybeUser) {
-                  case Some(user) if user.passwordMatches(password) =>
-                    val tokenGenerator = new BearerTokenGenerator
-                    val token = tokenGenerator.generateMD5Token(user.username)
-                    users += (token -> user)
-                    complete(LoginResponse(token, user.username))
-                  case _ => complete(StatusCodes.Unauthorized)
+      accessCtrlHeaders {
+        cors {
+          options {
+            complete(StatusCodes.OK)
+          } ~
+          pathPrefix("auth") {
+            path("login") {
+              post {
+                formFields('username.as[String], 'password.as[String]) { (username, password) =>
+                  val maybeUser = (todoService ? FindUser(username)).mapTo[Option[ApiUser]]
+                  onSuccess(maybeUser) {
+                    case Some(user) if user.passwordMatches(password) =>
+                      val tokenGenerator = new BearerTokenGenerator
+                      val token = tokenGenerator.generateMD5Token(user.username)
+                      users += (token -> user)
+                      complete(LoginResponse(token, user.username))
+                    case _ => complete(StatusCodes.Unauthorized)
+                  }
                 }
               }
             }
-          }
-        } ~
-        authenticateOAuth2(realm = "secure site", myUserPassAuthenticator) { user =>
-          pathPrefix("api") {
-            path("todos") {
-              get {
-                onComplete((todoService ? Todos(user.id)).mapTo[List[Todo]]) {
-                  case Success(todos)  => complete(todos)
-                  case Failure(ex) => complete(StatusCodes.InternalServerError, s"An error occurred: ${ex.getMessage}")
-                }
-              }
-            } ~
-            path("todo") {
-              post {
-                entity(as[Todo]) { todo =>
-                  var newTodo = todo
-                  if (todo.id == -1) {
-                    newTodo = Todo(scala.util.Random.nextInt, todo.description, todo.status, user.id)
-                  }
-                  onComplete((todoService ? CreateorUpdateTodo(newTodo))) {
-                    case Success(_)  => complete(StatusCodes.Created, newTodo)
+          } ~
+          authenticateOAuth2(realm = "secure site", myUserPassAuthenticator) { user =>
+            pathPrefix("api") {
+              path("todos") {
+                get {
+                  onComplete((todoService ? Todos(user.id)).mapTo[List[Todo]]) {
+                    case Success(todos) => complete(todos)
                     case Failure(ex) => complete(StatusCodes.InternalServerError, s"An error occurred: ${ex.getMessage}")
                   }
                 }
               } ~
-              delete {
-                parameters('id.as[Int]) { (id) =>
-                  onComplete((todoService ? DeleteTodobyId(id)).mapTo[Option[Todo]]) {
-                    case Success(maybeDeleted) => {
-                      maybeDeleted match {
-                        case Some(deleted) => complete(StatusCodes.OK, deleted)
-                        case None          => complete(StatusCodes.NotFound, s"Couldn't find todo by id ${id}")
-                      }
+              path("todo") {
+                post {
+                  entity(as[Todo]) { todo =>
+                    var newTodo = todo
+                    if (todo.id == -1) {
+                      newTodo = Todo(scala.util.Random.nextInt, todo.description, todo.status, user.id)
                     }
-                    case Failure(ex) => complete(StatusCodes.InternalServerError, s"An error occurred: ${ex.getMessage}")
+                    onComplete((todoService ? CreateorUpdateTodo(newTodo))) {
+                      case Success(_) => complete(StatusCodes.Created, newTodo)
+                      case Failure(ex) => complete(StatusCodes.InternalServerError, s"An error occurred: ${ex.getMessage}")
+                    }
+                  }
+                } ~
+                delete {
+                  parameters('id.as[Int]) { (id) =>
+                    onComplete((todoService ? DeleteTodobyId(id)).mapTo[Option[Todo]]) {
+                      case Success(maybeDeleted) => {
+                        maybeDeleted match {
+                          case Some(deleted) => complete(StatusCodes.OK, deleted)
+                          case None => complete(StatusCodes.NotFound, s"Couldn't find todo by id ${id}")
+                        }
+                      }
+                      case Failure(ex) => complete(StatusCodes.InternalServerError, s"An error occurred: ${ex.getMessage}")
+                    }
                   }
                 }
               }
--- src/test/scala/WebServiceSpec.scala
+++ src/test/scala/WebServiceSpec.scala
@@ -1,6 +1,7 @@
 package my.todos.test
 
 import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.headers.Origin
 import org.scalatest.{Matchers, WordSpec}
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import my.todos.RestService
@@ -9,7 +10,7 @@ class WebServiceSpec extends WordSpec with Matchers with ScalatestRouteTest with
 
   "WebService" should {
     "reject API requests without proper Authorization header" in {
-      Get("/api/todos") ~> route ~> check {
+      Get("/api/todos")~> Origin("http://localhost:3000") ~> route ~> check {
         handled shouldBe true
         status shouldEqual StatusCodes.Unauthorized
       }
